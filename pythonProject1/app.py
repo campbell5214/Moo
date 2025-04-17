0576d4c700da215cea837eb59a42977637ec561d
campbell5214 <campbell5214@hotmail.com>
2025-04-16 22:27:47 -0400

    Made more changes. Need to add more style!

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,7 +9,6 @@ def index():
 @app.route('/landing')
 def landing():
     return render_template('Landing.html')
-
 
 
 @app.route('/size', methods=['GET', 'POST'])
@@ -38,7 +37,6 @@ def sound():
     return render_template('Sound.html')
 
 
-
 @app.route('/color', methods=['GET', 'POST'])
 def color():
     if request.method == 'POST':
@@ -48,16 +46,31 @@ def color():
         width = float(request.form.get('width'))
         weight_lbs = float(request.form.get('weight_lbs'))
 
-        if length > 6.5 and width > 5.0 and weight_lbs > 880 and sound == "moo" and color == "brown":
-            return render_template('Yes.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
-        elif length < 3.3 or width < 2.6 or weight_lbs < 220:
-            return render_template('No.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
-        else:
-            return render_template('Maybe.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
+        # Redirect to the udders page to ask about udders
+        return render_template('Udders.html', length=length, width=width, weight_lbs=weight_lbs, sound=sound, color=color)
 
     return render_template('Color.html')
 
 
+@app.route('/udders', methods=['GET', 'POST'])
+def udders():
+    if request.method == 'POST':
+        udders = request.form.get('udders')
+        length = float(request.form.get('length'))
+        width = float(request.form.get('width'))
+        weight_lbs = float(request.form.get('weight_lbs'))
+        sound = request.form.get('sound')
+        color = request.form.get('color')
+
+        # Cow classification logic
+        if length > 6.5 and width > 5.0 and weight_lbs > 880 and sound == "moo" and color == "brown" and udders == "yes":
+            return render_template('Yes.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color, udders=udders)
+        elif length < 3.3 or width < 2.6 or weight_lbs < 220:
+            return render_template('No.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color, udders=udders)
+        else:
+            return render_template('Maybe.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color, udders=udders)
+
+    return render_template('Udders.html')
 
 
 @app.route('/yes')
