75a71b4a7ed7ccf3ac4df3efecab0722e04e6e68
campbell5214 <campbell5214@hotmail.com>
2025-04-16 21:51:03 -0400

    Functioning. Need to add in database and style!

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return redirect(url_for('landing'))
 
 @app.route('/landing')
 def landing():
@@ -15,10 +18,9 @@ def size():
         # Get size data from the form
         length = float(request.form.get('length'))
         width = float(request.form.get('width'))
-        weight = float(request.form.get('weight'))
+        weight_lbs = float(request.form.get('weight'))
 
-        # Store the size data to be passed along
-        return render_template('Sound.html', length=length, width=width, weight=weight)
+        return render_template('Sound.html', length=length, width=width, weight_lbs=weight_lbs)
 
     return render_template('Size.html')
 
@@ -26,40 +28,36 @@ def size():
 @app.route('/sound', methods=['GET', 'POST'])
 def sound():
     if request.method == 'POST':
-        # Get sound data from the form
         sound = request.form.get('sound')
         length = request.form.get('length')
         width = request.form.get('width')
-        weight = request.form.get('weight')
+        weight_lbs = request.form.get('weight_lbs')
 
-        # Pass the collected data to the next page (Color)
-        return render_template('Color.html', sound=sound, length=length, width=width, weight=weight)
+        return render_template('Color.html', sound=sound, length=length, width=width, weight_lbs=weight_lbs)
 
     return render_template('Sound.html')
+
 
 
 @app.route('/color', methods=['GET', 'POST'])
 def color():
     if request.method == 'POST':
-        # Get color data from the form
         color = request.form.get('color')
         sound = request.form.get('sound')
-        length = request.form.get('length')
-        width = request.form.get('width')
-        weight = request.form.get('weight')
+        length = float(request.form.get('length'))
+        width = float(request.form.get('width'))
+        weight_lbs = float(request.form.get('weight_lbs'))
 
-        # Decision-making logic based on all inputs
-        if float(length) > 2.0 and float(width) > 1.5 and float(weight) > 400 and sound == "moo" and color == "brown":
-            # It's a cow
-            return render_template('Yes.html', length=length, width=width, weight=weight, sound=sound, color=color)
-        elif float(length) < 1.0 or float(width) < 0.8 or float(weight) < 100:
-            # Definitely not a cow
-            return render_template('No.html', length=length, width=width, weight=weight, sound=sound, color=color)
+        if length > 6.5 and width > 5.0 and weight_lbs > 880 and sound == "moo" and color == "brown":
+            return render_template('Yes.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
+        elif length < 3.3 or width < 2.6 or weight_lbs < 220:
+            return render_template('No.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
         else:
-            # Maybe it's a cow (uncertain)
-            return render_template('Maybe.html', length=length, width=width, weight=weight, sound=sound, color=color)
+            return render_template('Maybe.html', length=length, width=width, weight=weight_lbs, sound=sound, color=color)
 
     return render_template('Color.html')
+
+
 
 
 @app.route('/yes')
